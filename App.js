25952cfd452ec27edcc0b1ca339a362ba1bfb7ab
seungho Jeon <urijsh612@gmail.com>
2019-11-06 16:12:27 +0900

    #1.1 Loading Screen

--- App.js
+++ App.js
@@ -1,30 +1,8 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.yellowView}>
-        <Text>노랑뷰</Text>
-      </View>
-      <View style={styles.redView}>
-        <Text>빨강뷰</Text>
-      </View>
-    </View>
-  );
-}
+import Loading from "./Loading";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "skyblue"
-  },
-  yellowView: {
-    flex: 1,
-    backgroundColor: "yellow"
-  },
-  redView: {
-    flex: 1,
-    backgroundColor: "red"
-  }
-});
+export default function App() {
+  return <Loading />;
+}
